553dc5a007aa0f79483c8647b374048c6cce28fa
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-12 17:44:59 +0900

    first commit

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,31 @@
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = 'sk_test_44d54dfcead84dd2c2bda30b'  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+end
---
+++ app/models/order.rb
@@ -0,0 +1,6 @@
+class Order < ApplicationRecord
+  attr_accessor :token
+
+  validates :price, presence: true
+  validates :token, presence: true
+end
